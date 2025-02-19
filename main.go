52039dbac1583ce55f3f554b5879c84e331d126f
Artem <arcelsium@gmail.com>
2025-02-20 01:56:57 +0300

    Добавление простого сортировщика запросов

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
@@ -19,7 +20,7 @@ func dbInit() (*gorm.DB, error) {
 	return db, nil
 }
 
-var filename string = "test_delete_all_comments.txt"
+var filename string = "coupleQueries.txt"
 var Line string = "_____________"
 
 func main() {
@@ -32,9 +33,60 @@ func main() {
 	content := string(data)
 	//? Очистка полученного файла от лишних комментариев
 	err = clearQueries(&content, "--")
-	fmt.Println(content)
 	if err != nil {
 		log.Println(err)
 		return
 	}
+
+	queries := strings.Split(content, ";")
+	fmt.Println(querySorter(&queries))
+}
+
+////  Сильно упрощённый перечень служебных слов, для определения назначения запроса
+//func getStartQueryWords() map[string]bool {
+//	words := make(map[string]bool)
+//	words["select"] = true
+//	words["create"] = true
+//	words["insert"] = true
+//	words["update"] = true
+//	words["delete"] = true
+//	words["with"] = true
+//	words["drop"] = true
+//	words["alter"] = true
+//	words["truncate"] = true
+
+//	return words
+//}
+
+// ? Сильно упрощённый сортировщик запросов
+func querySorter(queries *[]string) ([]string, []string, []string, []string) {
+	var create []string
+	var delete []string
+	var get []string
+	var others []string
+
+	//! Нереально упрощённая версия
+	for _, query := range *queries {
+		words := strings.Fields(strings.ToLower(query))
+		if len(words) == 0 {
+			others = append(others, query)
+			continue
+		}
+		firstWord := words[0]
+		switch firstWord {
+		case "select":
+			fmt.Println(1)
+			get = append(get, query)
+		case "create":
+			fmt.Println(2)
+			create = append(create, query)
+		case "delete":
+			fmt.Println(3)
+			delete = append(delete, query)
+		default:
+			fmt.Println(4)
+			others = append(others, query)
+		}
+	}
+	return create, delete, get, others
 }
